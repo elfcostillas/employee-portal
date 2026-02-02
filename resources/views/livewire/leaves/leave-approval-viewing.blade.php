9f9e3644094670e21aa2de99f580447d69dc4f5d
Elmer <elfcostillas@gmail.com>
2026-02-02 11:52:22 +0800

    leave approval return

--- resources/views/livewire/leaves/leave-approval-viewing.blade.php
+++ resources/views/livewire/leaves/leave-approval-viewing.blade.php
@@ -140,6 +140,7 @@
                 </table>
                 
             </div>
+            
         
             <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" type="button" class="px-5 py-2.5 text-sm font-medium text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
@@ -154,6 +155,20 @@
                 </svg>&emsp;
                 Deny
             </button>
+
+            @if(1==1)
+                <div class="mt-5">
+                    <button  onclick="window.location.href='{{ url('leave/leave-approval') }}'"  type="button" class="px-5 py-2.5 text-sm font-medium text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
+                            </svg>
+                            &emsp; &nbsp;&nbsp;
+                        Return
+                    </button>
+            
+                </div>
+            @endif
+            
             <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                 <div class="relative p-4 w-full max-w-md max-h-full">
                     <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
